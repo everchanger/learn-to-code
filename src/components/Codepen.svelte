b8f9e294d0468c739e4e669b74b6a8d08c1c764c
Joakim Rosenstam <joakim.rosenstam@gmail.com>
2022-04-17 15:29:06 +0200

    add codepen embeding

---
+++ src/components/Codepen.svelte
@@ -0,0 +1,11 @@
+<script>
+export let pen = 'MWrzMpY'
+export let username = 'everchanger-the-lessful'
+export let defaultTab = 'result'
+export let editable = 'true'
+
+console.log(pen)
+
+</script>
+
+<iframe height="300" style="width: 100%;" scrolling="no" title="Codepen" src={`https://codepen.io/${username}/embed/${pen}?default-tab=${defaultTab}&editable=${editable}`} frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true" />
