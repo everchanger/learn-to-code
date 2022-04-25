cfe5084978fe55cda6d134b255f7a34ddfb846a3
Joakim Rosenstam <joakim.rosenstam@gmail.com>
2022-04-25 19:22:51 +0200

    add first post!

--- src/components/Codepen.svelte
+++ src/components/Codepen.svelte
@@ -3,9 +3,6 @@ export let pen = 'MWrzMpY'
 export let username = 'everchanger-the-lessful'
 export let defaultTab = 'result'
 export let editable = 'true'
-
-console.log(pen)
-
 </script>
 
-<iframe height="300" style="width: 100%;" scrolling="no" title="Codepen" src={`https://codepen.io/${username}/embed/${pen}?default-tab=${defaultTab}&editable=${editable}`} frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true" />
+<iframe class="my-12" height="300" style="width: 100%;" scrolling="no" title="Codepen" src={`https://codepen.io/${username}/embed/${pen}?default-tab=${defaultTab}&editable=${editable}`} frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true" />
